a4ff9be647bdda9670644d0192fba39563c8d99b
Tyler Gross <tylergross28@gmail.com>
2020-07-28 13:12:24 -0500

    add emscripten

--- mac.sh
+++ mac.sh
@@ -1,6 +1,6 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install nvm yarn rustup-init
+brew install nvm yarn rustup-init emscripten
 brew cask install google-cloud-sdk visual-studio-code plex-media-server google-chrome firefox google-backup-and-sync intel-power-gadget steam github
 cd ~
 ssh-keygen
